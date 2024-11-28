ffa598a72e81b7c951dd8e60f592379a0d2d452e
ArmanWali <armanwali2019@gmail.com>
2024-11-28 22:52:16 +0500

    updating RPS game

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -15,42 +15,58 @@ function RandomNumber (){
     return computerMove;
 }
 
+const score = JSON.parse(localStorage.getItem('score'));
+
 function PlayGame(playerMove){
     const computerMove = RandomNumber();
     let result = '';
     if (playerMove == 'Rock'){
         if (computerMove === 'Rock'){
             result = 'It\'s a Tie.';
+            score.Ties += 1;
         }
         else if (computerMove === 'Paper'){
             result = 'You Lose.';
+            score.Losses += 1;
         }
         else if (computerMove === 'Scissors'){
             result = 'You win.';
+            score.Wins += 1;
         }
     }
     else if (playerMove == 'Paper'){
         if (computerMove === 'Rock'){
             result = 'You win.';
+            score.Wins += 1;
         }
         else if (computerMove === 'Paper'){
             result = 'It\'s a Tie';
+            score.Ties += 1;
         }
         else if (computerMove === 'Scissors'){
             result = 'You Lose.';
+            score.Losses += 1;
         }
     }
     else if (playerMove == 'Scissors'){
         if (computerMove === 'Rock'){
             result = 'You Lose.';
+            score.Losses += 1;
         }
         else if (computerMove === 'Paper'){
             result = 'You win.';
+            score.Wins += 1;
         }
         else if (computerMove === 'Scissors'){
             result = 'It\'s a Tie.';
+            score.Ties += 1;
         }
     }
 
-        alert(`You choose ${playerMove}, computer choose ${computerMove}, ${result}`);
+    localStorage.setItem('score', JSON.stringify(score));
+
+        alert(`You choose ${playerMove}, computer choose ${computerMove}, ${result}
+Wins: ${score.Wins}, Losses: ${score.Losses}, Ties: ${score.Ties}. 
+        `);
 }
+
